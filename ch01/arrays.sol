b7b0bcfd42ab769af24b8a6fd21ced733f9dc8da
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-01-14 23:07:21 +0900

    array

---
+++ ch01/arrays.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.11;
+
+contract ZombieFactory {
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies; 
+}
