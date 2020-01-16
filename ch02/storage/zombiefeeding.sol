2808bb29927a7695f8087e1c8fc59e565854429d
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-01-17 05:39:21 +0900

    storage

---
+++ ch02/storage/zombiefeeding.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.11;
+import "./zombiefactory.sol";
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+  }
+}
