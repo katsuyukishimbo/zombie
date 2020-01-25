9c822a0b475fae90aec68a53e38b41eafb5e6f75
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-01-25 21:40:31 +0900

    keccak256

--- ch04/attack/zombieattack.sol
+++ ch04/attack/zombieattack.sol
@@ -3,5 +3,10 @@ pragma solidity ^0.5.11;
 import "./zombiehelper.sol";
 
 contract ZombieBattle is ZombieHelper{
+  uint randNonce = 0;
 
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
 }
