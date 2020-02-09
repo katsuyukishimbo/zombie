1cc49c8749e2a19e2ead247f20f69f1e1ff9d103
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-02-09 21:09:29 +0900

    refactor

--- ch05/token/zombiefeeding.sol
+++ ch05/token/zombiefeeding.sol
@@ -18,7 +18,12 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  KittyInterface kittyContract;
+   KittyInterface kittyContract;
+
+  modifier onlyOwnerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
+  }
 
   function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
@@ -29,11 +34,10 @@ contract ZombieFeeding is ZombieFactory {
   }
 
   function _isReady(Zombie storage _zombie) internal view returns (bool) {
-    return (_zombie.readyTime <= now);
+      return (_zombie.readyTime <= now);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
