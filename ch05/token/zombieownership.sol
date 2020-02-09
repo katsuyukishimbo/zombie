c943f2b7c91b94f194371afff47c420627c35bd8
katsuyukishimbo <katsuyuki0611@gmail.com>
2020-02-09 21:33:12 +0900

    approve and takeOwnership

--- ch05/token/zombieownership.sol
+++ ch05/token/zombieownership.sol
@@ -5,6 +5,8 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  mapping (uint => address) zombieApprovals;
+
   function balanceOf(address _owner) public view returns (uint256 _balance) {
     return ownerZombieCount[_owner];
   }
@@ -20,15 +22,19 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     Transfer(_from, _to, _tokenId);
   }
 
-  function transfer(address _to, uint256 _tokenId) public {
-
+  function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+     _transfer(msg.sender, _to, _tokenId);
   }
 
   function approve(address _to, uint256 _tokenId) public {
-
+    zombieApprovals[_tokenId] = _to;
+    Approval(msg.sender, _to, _tokenId);
   }
 
   function takeOwnership(uint256 _tokenId) public {
-
+    require(zombieApprovals[_tokenId] == msg.sender);
+    address owner = ownerOf(_tokenId);
+    _transfer(owner, msg.sender, _tokenId);
   }
+}
 }
